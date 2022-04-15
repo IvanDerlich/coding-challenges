6b84c38d2f26aca60a22bae95d4559665ebe5059
Ivan Derlich <a@ivanderlich.com>
2022-04-15 13:27:13 -0300

    Beautify

--- AlgoExpert/15 Nth Fibonacci/sol1.js
+++ AlgoExpert/15 Nth Fibonacci/sol1.js
@@ -1,12 +1,12 @@
 function getNthFib(n) {
     if(n==1) return 0
-      if(n==2) return 1
-      let current = 1, last = 0, next, index = 2;
-      while(index != n){	
-          next = current + last
-          last = current
-          current = next		
-          index++
-      }
-      return next
+    if(n==2) return 1
+    let current = 1, last = 0, next, index = 2;
+    while(index != n){	
+        next = current + last
+        last = current
+        current = next		
+        index++
+    }
+    return next
   }
--- AlgoExpert/15 Nth Fibonacci/sol2.js
+++ AlgoExpert/15 Nth Fibonacci/sol2.js
@@ -1,5 +1,5 @@
 function getNthFib(n) {
     if(n==1) return 0
-      if(n==2) return 1
-      return getNthFib(n-1) + getNthFib(n-2)
+    if(n==2) return 1
+    return getNthFib(n-1) + getNthFib(n-2)
   }
