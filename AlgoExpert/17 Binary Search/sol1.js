d405a072f46f36da93ec6609832821b05824d7c7
Ivan Derlich <a@ivanderlich.com>
2022-04-30 20:11:58 -0300

    Binary search

---
+++ AlgoExpert/17 Binary Search/sol1.js
@@ -0,0 +1,22 @@
+function binarySearch(
+	array,
+	target,
+	startIndex=0,
+	endIndex=array.length-1
+) {
+  // Write your code here.
+	let middleIndex = startIndex + Math.ceil( (endIndex - startIndex) / 2)	
+	let middleNumber = array[middleIndex]
+	console.log("start",  "end", "middle", "middleNum", "target")
+	console.log(startIndex, endIndex, middleIndex, middleNumber, target)
+	
+	if( middleNumber === target){
+		return middleIndex
+	} else	if(startIndex === endIndex){
+		return -1
+	} else if (middleNumber > target){		
+		return binarySearch( array, target, startIndex, middleIndex - 1)
+	} else {		
+		return binarySearch( array, target, middleIndex, endIndex)
+	}
+}
