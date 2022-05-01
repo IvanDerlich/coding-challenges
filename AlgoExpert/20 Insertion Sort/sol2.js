4a48a4e4477daa6aafd27e40dea1e78171fb09c4
Ivan Derlich <a@ivanderlich.com>
2022-05-01 01:18:13 -0300

    Remove unnecesary

--- AlgoExpert/20 Insertion Sort/sol2.js
+++ AlgoExpert/20 Insertion Sort/sol2.js
@@ -1,6 +1,5 @@
 function insertionSort(array) {
   for(let i = 1 ; i < array.length; i++ ){		
-		if(array[i] > array[i-1]) continue
 		for(let j = i ; j > 0 ; j--){
 			if(array[j] >= array[j - 1]) break
 			let aux = array[j]
