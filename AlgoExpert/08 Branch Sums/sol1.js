25c6419c6306db8af5aeb859b9be684dcf8eb251
Ivan Derlich <a@ivanderlich.com>
2022-05-01 21:05:00 -0300

    Improve solution

--- AlgoExpert/08 Branch Sums/sol1.js
+++ AlgoExpert/08 Branch Sums/sol1.js
@@ -1,9 +1,7 @@
 function branchSums(root) {
-	console.log(root.value)
-	console.log("---")
 	if (root === null ) return
 	if (root.left === null && root.right === null)
-		return root.value
+		return [root.value]
 	return [
 		root.left ? branchSums(root.left) : undefined,
 		root.right ? branchSums(root.right) : undefined
