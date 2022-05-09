b20ee9cafeec05b0e91ef6f71b43a01992617cf1
Ivan Derlich <a@ivanderlich.com>
2022-05-08 23:11:52 -0300

    Improve Solution

---
+++ AlgoExpert/27 First Non-Repeating Character/sol2.js
@@ -0,0 +1,22 @@
+function firstNonRepeatingCharacter(string) {
+    const charCount = {}
+      for(let i = 0; i < string.length; i++){
+          let element = string[i]
+          if(charCount[element]) charCount[element]++
+          else charCount[element] = 1
+      }
+      const keys = Object.keys(charCount)
+      let firstNR
+      for(let i = 0; i < keys.length; i++){
+          const element = keys[i]
+          if(charCount[element]===1){
+              firstNR = element; 
+              break
+          }
+      }
+      if(!firstNR) return -1
+      for(let i = 0; i < string.length; i++){
+          const element = string[i]
+          if( element === firstNR) return i
+      }
+}
